aff5282b8870e5948236f215972e883aa03097ed
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-04-23 20:24:47 +0200

    Create Page Header

--- loan-application/src/App.js
+++ loan-application/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         
-      </header>
+      
     </div>
   );
 }
---
+++ loan-application/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <div className="logo">Logo</div>
+      <nav className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  );
+}
