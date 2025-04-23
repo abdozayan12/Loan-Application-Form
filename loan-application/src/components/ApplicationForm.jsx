3a1867eb2e8a6d3fe08b3f64592f5df12c0f37a0
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-04-23 21:39:21 +0200

    handle Form Inputs using state

--- loan-application/src/components/ApplicationForm.jsx
+++ loan-application/src/components/ApplicationForm.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function ApplicationForm() {
+    const [formInput, setFormInput] = useState({name: "", phone: "", age: "", employee: "false", salary: ""})
+
+
   function handleSubmit(e) {
     e.preventDefault();
-    alert("Form Submitted");
+      alert("Form Submitted");
+      setFormInput({
+      name: "",
+      phone: "",
+      age: "",
+      employee: false,
+      salary: ""
+    });
   }
 
   return (
@@ -12,30 +23,30 @@ export default function ApplicationForm() {
       <hr />
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
-        <input id="name" type="text" />
+        <input id="name" type="text" value={formInput.name} onChange={(e) => {setFormInput({...formInput, name: e.target.value})}} />
         <br />
 
         <label htmlFor="phone">Phone Number: </label>
-        <input id="phone" type="number" max='12'/>
+        <input id="phone"  maxLength="12" value={formInput.phone} onChange={(e) => {setFormInput({...formInput, phone: e.target.value})}}/>
         <br />
 
         <label htmlFor="age">Age: </label>
-        <input id="age" type="number" min="18" max="60" />
+        <input id="age" type="number" min="18" max="60" value={formInput.age} onChange={(e) => {setFormInput({...formInput, age: e.target.value})}} />
         <br />
 
         <label htmlFor="employee">Are You An Employee? </label>
-        <input id="employee" type="checkbox" />
+        <input id="employee" type="checkbox" value={formInput.employee} onChange={(e) => {setFormInput({...formInput, employee: e.target.checked})}} />
         <br />
 
         <label htmlFor="salary">Salary: </label>
-        <select id="salary">
-          <option>-- Select Salary --</option>
+        <select id="salary" value={formInput.salary} onChange={(e) => {setFormInput({...formInput, salary: e.target.value})}}>
+          <option value="">-- Select Salary --</option>
           <option>Less than 500$</option>
           <option>More than 500$</option>
         </select>
         <br /><br />
 
-        <button type="submit" disabled>Submit</button>
+        <button type="submit" >Submit</button>
       </form>
     </div>
   )
